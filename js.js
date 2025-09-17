687a21669df029fb90c1d14fe766f24067f13604
Juan Pablo Rios <juanpabloriosrios4@gmail.com>
2025-09-17 16:43:30 -0500

    se genera logica de calculadoras

--- js.js
+++ js.js
@@ -6,11 +6,22 @@ BotonMenu.addEventListener("click", () => {
   BotonMenu.classList.toggle("active");
 });
 
+const select = document.getElementById("herramienta");
+const calculadoras = document.querySelectorAll(".calculadora");
+
+select.addEventListener("change", function () {
+  calculadoras.forEach((calc) => (calc.style.display = "none"));
+  if (this.value) {
+    document.getElementById(this.value).style.display = "block";
+  }
+});
+//calculadora retefuente//
+
 document.getElementById("calcular").addEventListener("click", function () {
   let ingreso = Number(document.getElementById("ingreso").value);
   let deducciones = Number(document.getElementById("deducciones").value);
   let base = ingreso - deducciones;
-  let valorUVT = 48000;
+  const valorUVT = 49799;
   let baseUVT = base / valorUVT;
   function calculo(base, baseUVT) {
     let retencion = 0;
@@ -33,5 +44,51 @@ document.getElementById("calcular").addEventListener("click", function () {
     <p><strong>Base en UVT:</strong> ${baseUVT.toFixed(2)}</p>
     <p><strong>Retención:</strong> ${retencion.toLocaleString()}</p>
     <p><strong>Ingreso neto:</strong> ${neto.toLocaleString()}</p>
+  `;
+});
+
+//calculadora uvt
+const valorEnPesos = document.getElementById("valor");
+const resultadoUVT = document.getElementById("resultado-uvt");
+const botonConvertir = document.getElementById("ConvertirUVT");
+
+botonConvertir.addEventListener("click", function () {
+  let valorIngresoUvt = Number(valorEnPesos.value);
+
+  let valorUvt = 49799;
+  let Res_Uvt = valorIngresoUvt / valorUvt;
+
+  resultadoUVT.innerHTML = `
+    <p><strong>Valor ingresado:</strong> ${valorIngresoUvt.toLocaleString()}</p>
+    <p><strong>Equivalente en UVT:</strong> ${Res_Uvt.toFixed(2)}</p>
+  `;
+});
+
+//calculadora prima
+// Calculadora Prima
+const botonPrima = document.getElementById("calculoPrima");
+const salarioInput = document.getElementById("salario");
+const diasInput = document.getElementById("dias");
+const resultadoPrima = document.getElementById("resultado-prima");
+
+botonPrima.addEventListener("click", function () {
+  let salario = Number(salarioInput.value);
+  let dias = Number(diasInput.value);
+
+  if (!salario || !dias || salario <= 0 || dias <= 0) {
+    resultadoPrima.innerHTML =
+      "<p style='color:red'>⚠️ Ingrese valores válidos.</p>";
+    return;
+  }
+
+  let prima = (salario * dias) / 360;
+
+  resultadoPrima.innerHTML = `
+   <p><strong>Salario ingresado:</strong> $${salario.toLocaleString()}</p>
+   <p><strong>Días trabajados:</strong> ${dias}</p>
+   <p><strong>Prima a recibir:</strong> $${prima.toLocaleString(undefined, {
+     minimumFractionDigits: 2,
+     maximumFractionDigits: 2,
+   })}</p>
   `;
 });
